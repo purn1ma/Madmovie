ef040ef17d3e07d1f986b971bb048df8a9215a0a
khaleesi <anuragshrivastava@Anurags-MacBook-Air.local>
2023-07-18 00:33:45 +0530

    update api_url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
-const API_URL="http://www.omdbapi.com?apikey=87c68b84";
+const API_URL="https://www.omdbapi.com?apikey=87c68b84";
 
 
 const App = () => {
